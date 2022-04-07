0160a0b226f023bf5302a3a18a10be842fc2ae32
Gizmo <maciek2.kesicki2@gmail.com>
2022-04-08 00:12:42 +0200

    fixed stones size bug - borad size too big for screen

--- app/src/main/java/com/example/gomoku/PlayerSetupActivity.java
+++ app/src/main/java/com/example/gomoku/PlayerSetupActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class PlayerSetupActivity extends AppCompatActivity {
+
+    boolean isBoardSize15 = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class PlayerSetupActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
 
         Button submitButton = (Button)findViewById(R.id.submitButton);
+
 
         submitButton.setOnClickListener(view -> {
             EditText playerBlack = (EditText)findViewById(R.id.playerBlackName);
@@ -29,11 +33,25 @@ public class PlayerSetupActivity extends AppCompatActivity {
             if(playerWhiteName.length() == 0)
                 playerWhiteName = "White";
 
+            Intent intent15 = new Intent(view.getContext(), GameActivity15.class);
+            Intent intent19 = new Intent(view.getContext(), GameActivity19.class);
 
-            Intent intent = new Intent(view.getContext(), GameActivity15.class);
-            intent.putExtra("playerBlackName", playerBlackName);
-            intent.putExtra("playerWhiteName", playerWhiteName);
-            startActivity(intent);
+            if(isBoardSize15) {
+                intent15.putExtra("playerBlackName", playerBlackName);
+                intent15.putExtra("playerWhiteName", playerWhiteName);
+                startActivity(intent15);
+            } else {
+                intent19.putExtra("playerBlackName", playerBlackName);
+                intent19.putExtra("playerWhiteName", playerWhiteName);
+                startActivity(intent19);
+            }
         });
+
+    }
+    public void onRadioButtonClicked(View view) {
+        if(view.getId() == R.id.button15)
+            isBoardSize15 = true;
+        else
+            isBoardSize15 = false;
     }
 }
